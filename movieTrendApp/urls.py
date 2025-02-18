f1fb0ff382b3721f35802a0ff806afd4b6a42350
Marho <joborfrederick0214@gmail.com>
2025-02-18 17:26:43 +0100

    page for home root /

--- movieTrendApp/urls.py
+++ movieTrendApp/urls.py
@@ -1,13 +1,19 @@
 """
 URLS for movietrendapp
 """
-
 from django.urls import path
+from django.shortcuts import redirect
 from .views import get_integration_json, telex_tick
 
 app_name = "movietrendapp"
 
 urlpatterns = [
+    # Redirect root path to external website (the Movie Database)
+    path("", lambda request: redirect("https://www.themoviedb.org")),  # Root path redirect
+
+    # Integration JSON for Telex
     path("integration.json", get_integration_json, name="telex_integration"),
+
+    # Telex tick endpoint to fetch and send trending movies
     path("tick/", telex_tick, name="telex_tick"),
 ]
